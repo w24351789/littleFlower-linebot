6803283e20742668aa5cba27975570307178c1a5
intumit <w24351789@gmail.com>
2019-04-24 11:38:23 +0800

    fixsss

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //以下的四列require裡的內容，請確認是否已經用npm裝進node.js
-var linebot = require('linebot');
-var express = require('express');
+const linebot = require('linebot');
+const express = require('express');
 const { google } = require('googleapis');
-const { OAuth2Client } = require("google-auth-library")
+
 // 用於辨識Line Channel的資訊
 const bot = linebot({
   channelId: '1566408570',
@@ -12,7 +12,7 @@ const bot = linebot({
 
 
 //底下輸入client_secret.json檔案的內容
-var myClientSecret = { "installed":
+const myClientSecret = { "installed":
 {"client_id":"724449545250-69efl9n814a920hav1bvab9qu0ke1k4n.apps.googleusercontent.com",
 "project_id":"cobalt-ship-238507",
 "auth_uri":"https://accounts.google.com/o/oauth2/auth",
@@ -22,7 +22,7 @@ var myClientSecret = { "installed":
 "redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
 
 
-const oauth2Client = new OAuth2Client(myClientSecret.installed.client_id,myClientSecret.installed.client_secret, myClientSecret.installed.redirect_uris[0]);
+const oauth2Client = new google.auth.OAuth2(myClientSecret.installed.client_id,myClientSecret.installed.client_secret, myClientSecret.installed.redirect_uris[0]);
 
 //底下輸入sheetsapi.json檔案的內容
 oauth2Client.credentials = {"access_token":"ya29.Glv0Bj-GvntUwoq1Eiw6h44S5WBTYja65gofkD1WoOiPa_41bFVXs4hjtmMuofXqhGSQbW_f2fZybk_6ZVGHMWIRaAtsBqnAKusbhu8Jyfbe_QRmFEnXwJMyonPv",
@@ -32,7 +32,7 @@ oauth2Client.credentials = {"access_token":"ya29.Glv0Bj-GvntUwoq1Eiw6h44S5WBTYja
 "expiry_date":1556009862658}
 
 //試算表的ID，引號不能刪掉
-var mySheetId='1VLX79AlBmlkqIJgDK2BRDkxK3venpoL1jselGGIhmc4';
+const mySheetId='1VLX79AlBmlkqIJgDK2BRDkxK3venpoL1jselGGIhmc4';
 
 var myQuestions=[];
 var users=[];
@@ -44,12 +44,14 @@ getQuestions();
 
 
 //這是讀取問題的函式
-function getQuestions() {
+const getQuestions = () => {
   const sheets = google.sheets('v4');
   sheets.spreadsheets.values.get({
      auth: oauth2Client,
      spreadsheetId: mySheetId,
-     range:encodeURI('question1'),
+     //range:encodeURI('question1'),
+     range: 'question1!A1:D2',
+     majorDimension: 'ROWS'
   }, function(err, response) {
      if (err) {
         console.log('讀取問題檔的API產生問題：' + err);
