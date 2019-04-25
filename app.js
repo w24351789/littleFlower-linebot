c051e9a0c0d85638a17ecd59c5a6171f02395234
intumit <w24351789@gmail.com>
2019-04-25 23:41:11 +0800

    12323

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ bot.on('message', function(event) {
          users[myId].replies=[];
       }
      
-      const myStep=users[myId].step;
+      let myStep=users[myId].step;
       if (myStep===-1)
          sendMessage(event,myQuestions[0][0]);
       else{
