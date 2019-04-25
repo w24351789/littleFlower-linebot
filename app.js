d677f3e7a6b9f36a11af8f2497dc443435c215ca
intumit <w24351789@gmail.com>
2019-04-25 23:29:38 +0800

    123

--- app.js
+++ app.js
@@ -58,7 +58,8 @@ function getQuestions(){
      }
      var rows = response.values;
      console.log("rows = " + rows)
-     console.log(JSON.stringify(response, null, 2));
+     //console.log(JSON.stringify(response, null, 2));
+     console.log(response.data.values)
      console.log("=============================")
      if (rows == 0) { //有問題TypeError: Cannot read property 'length' of undefined
         console.log('No data found.');
