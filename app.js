888887931bde57f920d8463380811212a4d744de
intumit <w24351789@gmail.com>
2019-05-24 15:56:59 +0800

    mystep

--- app.js
+++ app.js
@@ -153,13 +153,13 @@ bot.on('message', function(event) {
                questionAns = myQuestions[0][0];
                questionChosen.text = questionAns;
                event.reply(questionChosen);
-               users[myId].replies[myStep+2]=event.message.text;
+               //users[myId].replies[myStep+2]=event.message.text;
             }
             else{
                //最後一題答完後
                if (myStep==(totalSteps-1)) {
-                  //users[myId].replies[myStep+1]=event.message.text;
-                  //users[myId].replies[myStep+2] = userName;//自動讀取使用者的名字
+                  users[myId].replies[myStep+1]=event.message.text;
+                  users[myId].replies[myStep+2] = userName;//自動讀取使用者的名字
                   event.reply(giftCard);
                }
                else if (myStep > -1 && myStep < 7){//A至H
